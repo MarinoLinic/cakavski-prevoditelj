67aed9a7a2f20e62a1b903587295a9f53b4ac117
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2023-04-12 17:18:56 +0200

    4

--- src/App.tsx
+++ src/App.tsx
@@ -14,24 +14,86 @@ function App() {
 	const translateToCakavski = (input: string): string => {
 		const wordList = input.split(' ')
 		const translatedList = wordList.map((word) => {
-			const mapping = cakavskiRjecnik.find((item: Mapping) => item.stokavski === word)
+			const regex = /[?!.,-]/g
+			let punct = ''
+			let alt = false
+			const vowels = 'aeiou'
+
+			let cleanedWord = word.replace(regex, (match) => {
+				punct += match
+				return ''
+			})
+
+			const mapping = cakavskiRjecnik.find(
+				(item: Mapping) => item.stokavski === cleanedWord || item.stokavski === cleanedWord + 'i' // govoriti, govorit
+			)
+
 			if (mapping) {
-				return mapping.cakavski
+				return mapping.cakavski + punct
 			}
+
+			if (cleanedWord.endsWith('io')) {
+				cleanedWord = cleanedWord.slice(0, -2) + 'il'
+				alt = true
+			}
+
+			if (cleanedWord.endsWith('m')) {
+				cleanedWord = cleanedWord.slice(0, -1) + 'n'
+				alt = true
+			}
+
+			if (cleanedWord.includes('ije')) {
+				if (cleanedWord.endsWith('ije')) {
+					cleanedWord = cleanedWord.replace('ije', 'i')
+				} else {
+					cleanedWord = cleanedWord.replace('ije', 'e')
+				}
+				alt = true
+			}
+
+			for (let i = 0; i < vowels.length; i++) {
+				let targetChar = vowels[i]
+
+				if (cleanedWord.charAt(cleanedWord.length - 1) === targetChar) {
+					const vowelMapping = cakavskiRjecnik.find(
+						(item: Mapping) => item.stokavski.slice(0, -1) === cleanedWord.slice(0, -1)
+					)
+
+					if (vowelMapping) {
+						if (vowels.includes(vowelMapping.cakavski.charAt(vowelMapping.cakavski.length - 1))) {
+							return vowelMapping.cakavski.slice(0, -1) + targetChar + punct
+						}
+					}
+				}
+			}
+
+			if (alt) return cleanedWord + punct
+
 			return word
 		})
+
 		return translatedList.join(' ')
 	}
 
 	const translateToStokavski = (input: string): string => {
 		const wordList = input.split(' ')
 		const translatedList = wordList.map((word) => {
-			const mapping = cakavskiRjecnik.find((item: Mapping) => item.cakavski === word)
+			const regex = /[?!.,-]/g
+			let punct = ''
+
+			const cleanedWord = word.replace(regex, (match) => {
+				punct += match
+				return ''
+			})
+
+			const mapping = cakavskiRjecnik.find((item: Mapping) => item.cakavski === cleanedWord)
+
 			if (mapping) {
-				return mapping.stokavski
+				return mapping.stokavski + punct
 			}
 			return word
 		})
+
 		return translatedList.join(' ')
 	}
 
